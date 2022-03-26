5c08be1b2e2f67314c0ea2d6a2e4413bdbb85b83
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-26 11:03:07 -0500

    Add Product Schema and model

--- app.js
+++ app.js
@@ -12,25 +12,25 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.get('/', (req, res) => {
-  res.send('<h1>File Upload Starter</h1>');
+	res.send('<h1>File Upload Starter</h1>');
 });
 
 // middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
+	try {
+		await connectDB(process.env.MONGO_URI);
 
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 start();
