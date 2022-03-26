992515bcabb677fbca327cf9c6c960566804cce0
Adrian DeRose <adrian.derose6@gmail.com>
2022-03-26 15:50:28 -0500

    Upload images to Cloudinary and remove temp files

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const cloudinary = require('cloudinary');
+const fs = require('fs');
 
-const uploadProductImage = async (req, res) => {
+const uploadProductImageLocal = async (req, res) => {
 	if (!req.files) {
 		throw new CustomError.BadRequestError('No file uploaded');
 	}
@@ -28,6 +30,18 @@ const uploadProductImage = async (req, res) => {
 	return res
 		.status(StatusCodes.OK)
 		.json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+const uploadProductImage = async (req, res) => {
+	const result = await cloudinary.uploader.upload(
+		req.files.image.tempFilePath,
+		{
+			use_filename: true,
+			folder: 'file-upload',
+		}
+	);
+	fs.unlinkSync(req.files.image.tempFilePath);
+	return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
 };
 
 module.exports = {
